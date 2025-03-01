6ef0b8055e8820d18ccfd77287f2267dd01fc501
Anoop Surej <surejanoop@gmail.com>
2025-03-01 21:22:30 +0000

    feat: The First Commit (template spring project)

---
+++ src/main/java/stonenotes/StonenotesApplication.java
@@ -0,0 +1,13 @@
+package stonenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonenotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stonenotes/StonenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package stonenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
